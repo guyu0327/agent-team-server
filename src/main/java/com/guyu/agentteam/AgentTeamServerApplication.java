0156e0bde9a1c07aced424848fa186e86724e6da
lixiao <lixiao717535996@outlook.com>
2026-09-10 16:03:03 +0800

    chore(init): 配置Spring Boot项目结构

---
+++ src/main/java/com/guyu/agentteam/AgentTeamServerApplication.java
@@ -0,0 +1,13 @@
+package com.guyu.agentteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentTeamServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentTeamServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guyu/agentteam/AgentTeamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guyu.agentteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentTeamServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
